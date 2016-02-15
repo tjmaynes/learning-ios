586b3d83cff75503e62ec38b8a64b24e4f334a26
TJ Maynes <tjmaynes@outlook.com>
2016-02-14 19:50:54 -0500

    Added initial ChatChat project with README.md

---
+++ ChatChat/ChatChat/LoginViewController.swift
@@ -0,0 +1,54 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    var ref: Firebase!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = Firebase(url: "https://chatchat-90210.firebaseio.com/")
+    }
+
+    @IBAction func loginDidTouch(sender: AnyObject) {
+        ref.authAnonymouslyWithCompletionBlock { (error, authData) in
+            if error != nil { print(error.description); return }
+            self.performSegueWithIdentifier("LoginToChat", sender: nil)
+        }
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        super.prepareForSegue(segue, sender: sender)
+
+        let navVc = segue.destinationViewController as! UINavigationController
+
+        let chatVc = navVc.viewControllers.first as! ChatViewController
+
+        chatVc.senderId = ref.authData.uid
+        chatVc.senderDisplayName = ""
+    }
+    
+}
+
---
+++ ChatChat/README.md
@@ -0,0 +1,10 @@
+# ChatChat
+
+Learn how to do the following:
+- Set up the Firebase SDK and JSQMessagesViewController with CocoaPods.
+- Synchronize data in real time with the Firebase database.
+- Authenticate anonymously with Firebase.
+- Leverage JSQMessagesViewController for a complete chat UI.
+- Indicate when a user is typing.
+
+[Source](http://www.raywenderlich.com/122148/firebase-tutorial-real-time-chat)
