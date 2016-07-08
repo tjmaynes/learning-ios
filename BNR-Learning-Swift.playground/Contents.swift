5a3a0594da58a137ffb747aa8e17e7c53b41935e
TJ Maynes <tjmaynes@outlook.com>
2016-07-08 01:17:43 -0400

    Going to work on chapters 4 - 8 next

--- BNR-Learning-Swift.playground/Contents.swift
+++ BNR-Learning-Swift.playground/Contents.swift
@@ -2,14 +2,22 @@
 
 import UIKit
 
-// chapter 1
+// Chapter 1
+
+var str = "Hello, playground"
+str += "!"
+print(str)
+
+let firstName : String = "TJ"
+var lastName : String = "Maynes"
+print("\(firstName) \(lastName)")
+
+// Chapter 2
 
 // declare a variable called numberoflights of type int that starts out with the value of 4.
 let numberOfStoplights: Int = 4
 
-var population: Int
-population = 5422
-
+var population: Int = 5422
 var unemploymentRate: Int = 11
 
 let townName: String = "Knowhere"
@@ -18,14 +26,26 @@ let townName: String = "Knowhere"
 let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights with \(unemploymentRate)% unemployment rate."
 print(townDescription)
 
+var message: String = population > 10000 ? "\(population) is huge!" : "\(population) is small..."
 
-// chapter 2
+print(message)
 
-var message: String
+var hasPostOffice: Bool = true
 
 if population < 10000 {
-    message = "\(population) is huge!"
+    message = "\(population) is a small town!"
+} else if population >= 10000 && population < 50000 {
+    message = "\(population) is a medium town!"
+} else if population >= 50000 && population < 100000 {
+    message = "\(population) is a large town!"
 } else {
-    message = "\(population) is small..."
+    message = "\(population) is pretty big!"
 }
-print(message)
+
+if !hasPostOffice {
+    print("Where do we buy stamps!")
+}
+
+// Chapter 4
+
+
