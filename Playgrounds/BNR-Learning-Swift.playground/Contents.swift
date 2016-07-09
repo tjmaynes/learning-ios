18299d4efc473eb62025ca3226f3d73288a93389
TJ Maynes <tjmaynes@outlook.com>
2016-07-09 01:03:18 -0400

    Finished chapter 4

--- Playgrounds/BNR-Learning-Swift.playground/Contents.swift
+++ Playgrounds/BNR-Learning-Swift.playground/Contents.swift
@@ -48,4 +48,69 @@ if !hasPostOffice {
 
 // Chapter 4
 
+print("The maximum Int value is \(Int.max)")
+print("The minimum Int value is \(Int.min)")
+
+print("The maximum 32-bit Int value is \(Int32.max)")
+print("The minimum 32-bit Int value is \(Int32.min)")
+
+print("The maximum U-Int value is \(UInt.max)")
+print("The minimum U-Int value is \(UInt.min)")
+
+print("The maximum 32-bit U-Int value is \(UInt32.max)")
+print("The minimum 32-bit U-Int value is \(UInt32.min)")
+
+let numberOfPages: Int = 10
+let numberOfChapters = 3
+
+let numberOfPeople: UInt = 40
+let volumeAdjustment: Int32 = -1000
+
+print(10 + 20)
+print(30 - 5 * 2)
+print((5 * 6) / 2)
+print(-11 % 3)
+
+let y: Int8 = 120
+let z = y &+ 10
+
+let a: Int16 = 200
+let b: Int8 = 50
+let c = a + Int16(b)
+
+let d1 = 1.1
+let d2: Double = 1.1
+let f1: Float = 100.3
+
+print(10.0 + 11.4)
+print(11.0 / 3.0)
+
+if d1 == d2 {
+    print("d1 and d2 are the same")
+}
+
+print("d1 + 0.1 is \(d1 + 0.1)")
+if d1 + 0.1 == 1.2 {
+    print("d1 + 0.1 is equal to 1.2")
+}
+
+// Chapter 5
+
+var statusCode: Int = 204
+var errorString: String = "The request failed with the error:"
+switch statusCode {
+case 100, 101:
+    errorString += " Informational, \(statusCode)."
+case 204:
+    errorString += " Successful but no content"
+case 300...307:
+    errorString += " Redirection, \(statusCode)."
+case 400...417:
+    errorString += " Client error, \(statusCode)."
+case 500...505:
+    errorString += " Server error, \(statusCode)."
+default:
+    errorString = "\(statusCode) is not a known error code."
+}
+
 
