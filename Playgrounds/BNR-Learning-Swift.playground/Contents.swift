7f70d0d423cf663e6e5d84c9058924fc1a5cfede
TJ Maynes <tjmaynes@outlook.com>
2016-09-28 01:36:26 -0400

    Working on Chapter 12 now

--- Playgrounds/BNR-Learning-Swift.playground/Contents.swift
+++ Playgrounds/BNR-Learning-Swift.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-// Chapter 1
+// Chapter 1 - Getting Started
 
 var str = "Hello, playground"
 str += "!"
@@ -12,7 +12,7 @@ let firstName : String = "TJ"
 var lastName : String = "Maynes"
 print("\(firstName) \(lastName)")
 
-// Chapter 2
+// Chapter 2 - Types, Constants, and Variables
 
 // declare a variable called numberoflights of type int that starts out with the value of 4.
 let numberOfStoplights: Int = 4
@@ -25,6 +25,8 @@ let townName: String = "Knowhere"
 // \() is a place for a string literal
 let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights with \(unemploymentRate)% unemployment rate."
 print(townDescription)
+
+// Chapter 3 - Conditionals If/Else
 
 var message: String = population > 10000 ? "\(population) is huge!" : "\(population) is small..."
 
@@ -46,7 +48,7 @@ if !hasPostOffice {
     print("Where do we buy stamps!")
 }
 
-// Chapter 4
+// Chapter 4 - Numbers
 
 print("The maximum Int value is \(Int.max)")
 print("The minimum Int value is \(Int.min)")
@@ -94,7 +96,7 @@ if d1 + 0.1 == 1.2 {
     print("d1 + 0.1 is equal to 1.2")
 }
 
-// Chapter 5
+// Chapter 5 - Swift Statements
 
 var statusCode: Int = 418
 var errorString: String = "The request failed with the error:"
@@ -135,7 +137,7 @@ default:
 }
 
 let age: Int = 25
-if case 18...35 = age where age >= 21 {
+if case 18...35 = age, age >= 21 {
     print("In cool demographic and of drinking age")
 }
 
@@ -157,7 +159,7 @@ default:
     print("Case not covered")
 }
 
-// Chapter 6
+// Chapter 6 - Loops
 
 var myFirstInt: Int = 0
 for _ in 1...5 {
@@ -175,6 +177,7 @@ while i > 6 {
     print(myFirstInt)
 }
 
+/*
 var shields = 5
 var blastersOverheating = false
 var blasterFireCount = 0
@@ -203,6 +206,7 @@ while shields > 0 {
     blasterFireCount += 1
     spaceDemonsDestroyed += 1
 }
+*/
 
 for _ in 1...5 {
     for case let i in 1...100 where i % 2 == 0 {
@@ -210,7 +214,7 @@ for _ in 1...5 {
     }
 }
 
-// Chapter 7
+// Chapter 7 - Strings
 
 let playground = "Hello, playground"
 var mutablePlayground = "Hello, mutable playground"
@@ -235,5 +239,101 @@ print("aAcuteDecomposed: \(aAcutePrecomposed.characters.count)")
 let fromStart = playground.startIndex
 let toPosition = 4
 
+// Chapter 8 - Optionals
 
+/*
+ Optionals are a special feature of Swift used to indicate that an instance may not have a value. When you see an optional, you know one of two things about that instance: either it has a value and it is ready for use, or it has no value.
+*/
+
+var errorCodeString: String?
+//errorCodeString = "404"
+if errorCodeString != nil {
+    let theError = errorCodeString! // forced unwrapping - assume a value exists
+    print(theError)
+}
+
+// nested optional binding
+if let theError = errorCodeString, let errorCodeInteger = Int(theError), errorCodeInteger == 404 {
+    print("\(theError): \(errorCodeInteger)")
+}
+
+var errorDescription: String?
+if let theError = errorCodeString, let errorCodeInteger = Int(theError), errorCodeInteger == 404 {
+    errorDescription = ("\(errorCodeInteger + 200): the requested resouce was not found.")
+}
+
+var upCaseErrorDescription = errorDescription?.uppercased() // optional chaining
+upCaseErrorDescription?.append("PLEASE TRY AGAIN") // modifying in-place, note why we don't use !. forced unwrapping is dangerous
+
+errorDescription = nil
+let description = errorDescription ?? "No error" // nil coalescing operator
+
+let badDecision : String!
+//print(badDecision)
+
+// Chapter 9 - Arrays
+
+var movieRatings = ["Donnie Darko": 4, "Twister": 4, "Dark City": -1]
+print("I have rated \(movieRatings.count) movies")
+
+let darkoRating = movieRatings["Donnie Darko"]
+movieRatings["Dark City"] = 5
+
+let oldRating: Int? = movieRatings.updateValue(5, forKey: "Donnie Darko")
+if let lastRating = oldRating, let currentRating = movieRatings["Donnie Darko"] {
+    print("Old rating: \(lastRating); current rating: \(currentRating);")
+}
+
+// Chapter 10 - Dictionaries
+
+movieRatings.removeValue(forKey: "Dark City")
+movieRatings["Dark City"] = nil
+
+for (key, value) in movieRatings {
+    print("\(key) - \(value)")
+}
+
+for movie in movieRatings.keys {
+    print("User has rated \(movie)")
+}
+
+let watchedMovies = Array(movieRatings.keys)
+
+let georgia = ["macon": (30306, 30307, 30309), "atlanta": (30011, 30305, 30010)]
+for zipCodes in georgia.values {
+    print("\(zipCodes)")
+}
+
+// Chapter 11 - Sets
+
+var groceryBag : Set = ["Apples", "Oranges", "Pineapple"]
+//groceryBag.insert("Apples")
+//groceryBag.insert("Oranges")
+//groceryBag.insert("Pineapple")
+
+for food in groceryBag {
+    print(food)
+}
+
+let hasBananas = groceryBag.contains("Bananas")
+let friendsGroceryBag = Set(["Bananas", "Cereal", "Milk", "Oranges"])
+let commonGroceryBag = groceryBag.union(friendsGroceryBag)
+groceryBag.formUnion(friendsGroceryBag)
+print(groceryBag)
+
+let roommatesGroceryBag = Set(["Apples", "Bananas", "Cereal", "Toothpaste"])
+let itemsToReturn = commonGroceryBag.intersection(roommatesGroceryBag)
+groceryBag.formIntersection(roommatesGroceryBag)
+print(groceryBag)
+
+let yourSecondBag = Set(["Berries", "Yogurt"])
+let roommatesSecondBag = Set(["Grapes", "Honey"])
+let disjoint = yourSecondBag.isDisjoint(with: roommatesSecondBag)
+
+let myCities = Set(["Atlanta", "Chicago", "Jacksonville", "New York", "San Francisco"])
+let yourCities = Set(["Chicago", "San Francisco", "Jacksonville"])
+
+let containsAllMyCities = myCities.isSuperset(of: yourCities)
+
+// Chapter 12 - Functions
 
